8ea83099bec5dbf6d2cd56e86186b3d5c577c9a5
Suryana <nanasuryana335@gmail.com>
2025-06-30 22:52:10 +0700

    feat: create new model (product)

---
+++ models/product.go
@@ -0,0 +1,22 @@
+package models
+
+type (
+	Product struct {
+		ID int64 `json:"id"`
+		Name string `json:"name"`
+		Description string `json:"description"`
+		Price float64 `json:"price"`
+		Stock int `json:"stock"`
+		Category_ID int64 `json:"category_id"`
+	}
+
+	ProductCategory struct {
+		ID int `json:"id"`
+		Name string `json:"name"`
+	}
+
+	ProductCategoryManagementParameter struct {
+		Action string `json:"action"`
+		ProductCategory
+	}
+)
